f5ab474ef0a9f9e1b617c551e240dafc2389023f
John Vidler <git@johnvidler.co.uk>
2019-09-24 11:10:42 +0100

    Expanded the round-robin test to include more sinks, just for variety

--- tests/RoundRobinTest.sh
+++ tests/RoundRobinTest.sh
@@ -9,16 +9,20 @@ do
 
     ./GraphRouter &
 
-    sleep 2
+    sleep 1
 
     ./Graph -a 2000 -o | ../../modified-pv/pv -lrafb -t -i 1 2> 2000.log > /dev/null &
     ./Graph -a 3000 -o | ../../modified-pv/pv -lrafb -t -i 1 2> 3000.log > /dev/null &
+    ./Graph -a 4000 -o | ../../modified-pv/pv -lrafb -t -i 1 2> 4000.log > /dev/null &
+    ./Graph -a 5000 -o | ../../modified-pv/pv -lrafb -t -i 1 2> 5000.log > /dev/null &
     ./ArgTest -p 4 -i 0 -s 128  -c ${LENGTH} | ../../modified-pv/pv -lrafb -t -i 1 2> 1000.log | ./Graph -a 1000 -i && killall GraphRouter &
 
-    sleep 2
+    sleep 1
 
     ./GraphRouter -s 1000 -t 2000 -c &
     ./GraphRouter -s 1000 -t 3000 -c &
+    ./GraphRouter -s 1000 -t 4000 -c &
+    ./GraphRouter -s 1000 -t 5000 -c &
     ./GraphRouter -t 1000 --policy roundrobin &
 
     wait
@@ -26,17 +30,21 @@ do
     killall GraphRouter
     killall Graph
 
-    sleep 2
+    sleep 1
 
     cat 1000.log | tr '\r' '\n' | sed 's/ /,/g' > "./data/${LENGTH}/1000.log"
     cat 2000.log | tr '\r' '\n' | sed 's/ /,/g' > "./data/${LENGTH}/2000.log"
     cat 3000.log | tr '\r' '\n' | sed 's/ /,/g' > "./data/${LENGTH}/3000.log"
+    cat 4000.log | tr '\r' '\n' | sed 's/ /,/g' > "./data/${LENGTH}/3000.log"
+    cat 5000.log | tr '\r' '\n' | sed 's/ /,/g' > "./data/${LENGTH}/3000.log"
 
     rm 1000.log
     rm 2000.log
     rm 3000.log
+    rm 4000.log
+    rm 5000.log
 
-    sleep 2
+    sleep 1
 done
 
 echo "Done!"
